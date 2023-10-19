ebacafce913c5259b52e8d829547c632c83b0168
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2023-10-19 13:51:01 +0330

    add api list

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 import { BillboardColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -31,6 +32,8 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API calls for billboards" />
+      <ApiList entityIdName="bilboardId" entityName="billboards" />
     </>
   );
 };
